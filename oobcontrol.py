91c45e631122ce4c9074e95fa6c002a6a0677aea
George Prekas <prekgeo@yahoo.com>
2018-08-22 15:24:20 +0100

    various fixes

--- oobcontrol.py
+++ oobcontrol.py
@@ -9,6 +9,7 @@ import ssl
 import subprocess
 import sys
 import urllib2
+import xml.dom.minidom
 
 def java(params):
   try:
@@ -23,7 +24,6 @@ class Base:
     self.hostname = hostname
     self.username = username
     self.password = password
-    self.check_for_missing_jars()
 
   def check_for_missing_jars(self):
     if os.path.exists(os.path.basename(self.FILES[0])):
@@ -50,33 +50,50 @@ class Idrac(Base):
   ]
 
   def console(self):
+    self.check_for_missing_jars()
     java('-cp avctKVM.jar com.avocent.idrac.kvm.Main ip=%s user=%s passwd=%s kmport=5900 vport=5900 apcp=1' % (self.hostname, self.username, self.password))
 
-  def reboot(self):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+  def _login(self):
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
     cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPSHandler(context = context), urllib2.HTTPCookieProcessor(cj))
+    self.opener = urllib2.build_opener(urllib2.HTTPSHandler(context = context), urllib2.HTTPCookieProcessor(cj))
     
-    f = opener.open('https://%s/data/login' % self.hostname, 'user=%s&password=%s' % (self.username, self.password))
+    f = self.opener.open('https://%s/data/login' % self.hostname, 'user=%s&password=%s' % (self.username, self.password))
     data = f.read()
     forward_url = re.search(r'forwardUrl>([^<]*)', data)
     if forward_url is None:
       error('Cannot login')
     forward_url = forward_url.group(1)
 
+    token_name = re.search(r',([^=]*)', forward_url).group(1)
+    token_value = re.search(r'([^=]*)$', forward_url).group(1)
+    self.opener.addheaders.append((token_name, token_value))
+    
+  def reboot(self):
     try:
-      f = opener.open('https://%s/%s' % (self.hostname, forward_url))
-      data = f.read()
-      token_name = re.search(r'TOKEN_NAME = "([^"]*)', data).group(1)
-      token_value = re.search(r'TOKEN_VALUE = "([^"]*)', data).group(1)
-      opener.addheaders.append((token_name, token_value))
-      f = opener.open('https://%s/data?set=pwState:3' % self.hostname, '')
+      self._login()
+      f = self.opener.open('https://%s/data?set=pwState:3' % self.hostname, '')
       data = f.read()
       if '<status>ok</status>' not in data:
         error('Cannot reboot')
     finally:
-      f = opener.open('https://%s/data/logout' % self.hostname)
+      f = self.opener.open('https://%s/data/logout' % self.hostname)
       f.read()
+  
+  def disconnect(self):
+    self._login()
+    f = self.opener.open('https://%s/data?get=activeSessions' % self.hostname, '')
+    data = f.read()
+    doc = xml.dom.minidom.parseString(data)
+    for session in doc.getElementsByTagName('session'):
+      sessionId = session.getElementsByTagName('sessionId')[0].firstChild.nodeValue
+      sessionType = session.getElementsByTagName('sessionType')[0].firstChild.nodeValue
+      if sessionType == 'vKVM':
+        print 'Disconnecting session %s...' % sessionId
+        f = self.opener.open('https://%s/data?set=killSession(%s)' % (self.hostname, sessionId), '')
+        f.read()
 
 class Intel(Base):
   FILES = [
@@ -88,6 +105,7 @@ class Intel(Base):
   ]
 
   def console(self):
+    self.check_for_missing_jars()
     opener = urllib2.build_opener()
 
     session_cookie = self.login(opener)
@@ -102,7 +120,9 @@ class Intel(Base):
     java('-cp JViewer.jar com.ami.kvm.jviewer.JViewer %s 7578 %s 0 0 0 5120 5123 255 ":Ctrl+Alt+Del:Ctrl Alt Del:Alt+Tab:Alt Tab" EN %s' % (self.hostname, arg3, session_cookie))
 
   def reboot(self):
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPSHandler(context = context), urllib2.HTTPCookieProcessor(cj))
 
@@ -167,13 +187,16 @@ def main():
   config = read_config()
 
   while True:
-    action = raw_input('(c)onsole or (r)eboot ? ')
+    action = raw_input('(c)onsole, (r)eboot, or (d)isconnect ? ')
     action = action.lower()
     if action == 'c':
       select_machine().console()
       break
     elif action == 'r':
       select_machine().reboot()
+      break
+    elif action == 'd':
+      select_machine().disconnect()
       break
 
 if __name__ == '__main__':
